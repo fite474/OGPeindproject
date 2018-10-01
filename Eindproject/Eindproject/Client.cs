4b89d27290ccb8145a7145d86be848c836d14705
Unknown <mauricebrouwers373@hotmail.com>
2018-10-01 13:05:28 +0200

    and more stufff

--- Eindproject/Eindproject/Client.cs
+++ Eindproject/Eindproject/Client.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace Eindproject
 {
-    class Program
+    class Client
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //420
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private void Run() {
+
         }
     }
 }
--- Eindproject/Server/HandleClientThread.cs
+++ Eindproject/Server/HandleClientThread.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace Server
 {
     class HandleClientThread
     {
+        public HandleClientThread(object obj1, object obj2) {
+            TcpClient client1 = obj1 as TcpClient;
+            TcpClient client2 = obj2 as TcpClient;
+            int x = 0;
+
+        }
+
+
+        private void WriteTextMessage() {
+
+        }
     }
 }
