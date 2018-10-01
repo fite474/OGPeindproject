436eb8de284475fb88a8c4526f0ddc9659602426
Unknown <mauricebrouwers373@hotmail.com>
2018-10-01 15:29:39 +0200

    /serverbouwen: Auto stash before merge of "design/serverbouwen" and "origin/design/serverbouwen"

--- Eindproject/Eindproject/GameClient.Designer.cs
+++ Eindproject/Eindproject/GameClient.Designer.cs
@@ -29,34 +29,58 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rockbutton = new System.Windows.Forms.Button();
+            this.paperbutton = new System.Windows.Forms.Button();
+            this.scissorbutton = new System.Windows.Forms.Button();
+            this.pockbutton = new System.Windows.Forms.Button();
+            this.lizardbutton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.scoreplayer1 = new System.Windows.Forms.Label();
+            this.scoreplayer2 = new System.Windows.Forms.Label();
+            this.player1choice = new System.Windows.Forms.PictureBox();
+            this.player2choice = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player1choice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2choice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 6;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.05608F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.94392F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30.43478F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 69.56522F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 146F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 148F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 139F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 143F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.rockbutton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.paperbutton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.scissorbutton, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pockbutton, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.lizardbutton, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.scoreLabel, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.scoreplayer1, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.scoreplayer2, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.player1choice, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.player2choice, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox3, 3, 4);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 43.06569F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56.93431F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 96F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 85F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 90F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 81F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(776, 426);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(945, 563);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // button1
+            // rockbutton
             // 
+<<<<<<< Updated upstream
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.Location = new System.Drawing.Point(3, 45);
             this.button1.Name = "button1";
@@ -65,16 +89,148 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+=======
+            this.rockbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rockbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rockbutton.Location = new System.Drawing.Point(3, 93);
+            this.rockbutton.Name = "rockbutton";
+            this.rockbutton.Size = new System.Drawing.Size(106, 114);
+            this.rockbutton.TabIndex = 0;
+            this.rockbutton.Text = "button1";
+            this.rockbutton.UseVisualStyleBackColor = true;
+            this.rockbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // paperbutton
+            // 
+            this.paperbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.paperbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.paperbutton.Location = new System.Drawing.Point(3, 213);
+            this.paperbutton.Name = "paperbutton";
+            this.paperbutton.Size = new System.Drawing.Size(106, 90);
+            this.paperbutton.TabIndex = 1;
+            this.paperbutton.Text = "button2";
+            this.paperbutton.UseVisualStyleBackColor = true;
+            this.paperbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // scissorbutton
+            // 
+            this.scissorbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scissorbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scissorbutton.Location = new System.Drawing.Point(3, 309);
+            this.scissorbutton.Name = "scissorbutton";
+            this.scissorbutton.Size = new System.Drawing.Size(106, 79);
+            this.scissorbutton.TabIndex = 2;
+            this.scissorbutton.Text = "button3";
+            this.scissorbutton.UseVisualStyleBackColor = true;
+            this.scissorbutton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pockbutton
+            // 
+            this.pockbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pockbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pockbutton.Location = new System.Drawing.Point(3, 394);
+            this.pockbutton.Name = "pockbutton";
+            this.pockbutton.Size = new System.Drawing.Size(106, 84);
+            this.pockbutton.TabIndex = 3;
+            this.pockbutton.Text = "button4";
+            this.pockbutton.UseVisualStyleBackColor = true;
+            this.pockbutton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lizardbutton
+            // 
+            this.lizardbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lizardbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.lizardbutton.Location = new System.Drawing.Point(3, 484);
+            this.lizardbutton.Name = "lizardbutton";
+            this.lizardbutton.Size = new System.Drawing.Size(106, 76);
+            this.lizardbutton.TabIndex = 4;
+            this.lizardbutton.Text = "button5";
+            this.lizardbutton.UseVisualStyleBackColor = true;
+            this.lizardbutton.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(517, 38);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(142, 13);
+            this.scoreLabel.TabIndex = 5;
+            this.scoreLabel.Text = "Score";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreplayer1
+            // 
+            this.scoreplayer1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.scoreplayer1.AutoSize = true;
+            this.scoreplayer1.Location = new System.Drawing.Point(665, 38);
+            this.scoreplayer1.Name = "scoreplayer1";
+            this.scoreplayer1.Size = new System.Drawing.Size(133, 13);
+            this.scoreplayer1.TabIndex = 6;
+            this.scoreplayer1.Text = "player1";
+            // 
+            // scoreplayer2
+            // 
+            this.scoreplayer2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.scoreplayer2.AutoSize = true;
+            this.scoreplayer2.Location = new System.Drawing.Point(804, 38);
+            this.scoreplayer2.Name = "scoreplayer2";
+            this.scoreplayer2.Size = new System.Drawing.Size(138, 13);
+            this.scoreplayer2.TabIndex = 7;
+            this.scoreplayer2.Text = "label1";
+            // 
+            // player1choice
+            // 
+            this.player1choice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player1choice.Location = new System.Drawing.Point(371, 309);
+            this.player1choice.Name = "player1choice";
+            this.player1choice.Size = new System.Drawing.Size(140, 79);
+            this.player1choice.TabIndex = 8;
+            this.player1choice.TabStop = false;
+            this.player1choice.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // player2choice
+            // 
+            this.player2choice.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.player2choice.Location = new System.Drawing.Point(665, 309);
+            this.player2choice.Name = "player2choice";
+            this.player2choice.Size = new System.Drawing.Size(133, 79);
+            this.player2choice.TabIndex = 9;
+            this.player2choice.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(517, 394);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(142, 84);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+>>>>>>> Stashed changes
             // 
             // GameClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(995, 587);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "GameClient";
             this.Text = "GameClient";
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player1choice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player2choice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -82,6 +238,16 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button rockbutton;
+        private System.Windows.Forms.Button paperbutton;
+        private System.Windows.Forms.Button scissorbutton;
+        private System.Windows.Forms.Button pockbutton;
+        private System.Windows.Forms.Button lizardbutton;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label scoreplayer1;
+        private System.Windows.Forms.Label scoreplayer2;
+        private System.Windows.Forms.PictureBox player1choice;
+        private System.Windows.Forms.PictureBox player2choice;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- Eindproject/Eindproject/GameClient.cs
+++ Eindproject/Eindproject/GameClient.cs
@@ -14,16 +14,59 @@ namespace Eindproject
 {
     public partial class GameClient : Form
     {
-        
+        string awnser;
         public GameClient()
         {
             InitializeComponent();
+<<<<<<< Updated upstream
             this.button1.BackgroundImage = Eindproject.Properties.Resource1.lizard;
+=======
+            this.rockbutton.BackgroundImage = Eindproject.Properties.Resource1.rock;
+            this.paperbutton.BackgroundImage = Eindproject.Properties.Resource1.paper;
+            this.scissorbutton.BackgroundImage = Eindproject.Properties.Resource1.scissors;
+            this.pockbutton.BackgroundImage = Eindproject.Properties.Resource1.spock;
+            this.lizardbutton.BackgroundImage = Eindproject.Properties.Resource1.lizard;
+            player1choice.Image = Eindproject.Properties.Resource1.lizard;
+            player2choice.Image = Eindproject.Properties.Resource1.lizard;
+>>>>>>> Stashed changes
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
             
+=======
+            awnser = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            awnser = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            awnser = "";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            awnser = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            awnser = "";
+        }
+
+        public string GetAwnser() {
+            return awnser;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+>>>>>>> Stashed changes
         }
     }
 }
