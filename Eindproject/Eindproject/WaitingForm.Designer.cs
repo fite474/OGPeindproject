849951b5525e73ac5812f1f0d1a4a33cc598794e
Marck420 <m.debont1@student.avans.nl>
2018-10-08 15:44:14 +0200

    [Added] the waiting form for player 2

---
+++ Eindproject/Eindproject/WaitingForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Eindproject
+{
+    partial class WaitingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WaitingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WaitingLabel
+            // 
+            this.WaitingLabel.AutoSize = true;
+            this.WaitingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.WaitingLabel.Location = new System.Drawing.Point(100, 42);
+            this.WaitingLabel.MaximumSize = new System.Drawing.Size(100, 100);
+            this.WaitingLabel.Name = "WaitingLabel";
+            this.WaitingLabel.Size = new System.Drawing.Size(83, 72);
+            this.WaitingLabel.TabIndex = 0;
+            this.WaitingLabel.Text = "Waiting for player 1 to choose a gamemode";
+            this.WaitingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WaitingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.ControlBox = false;
+            this.Controls.Add(this.WaitingLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "WaitingForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Waiting...";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.WaitingForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label WaitingLabel;
+    }
+}
